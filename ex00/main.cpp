6b53166c8a7a3a7efb6f0d80ee19ccd2acda3e33
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-06-13 13:57:31 +0400

    ex00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 13:32:37 by akeryan           #+#    #+#             */
-/*   Updated: 2024/06/13 13:40:50 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/06/13 13:56:42 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,21 @@
 
 int main(void)
 {
-	int a = 7;
-	int b = 9;
+	int a = 2;
+	int b = 3;
 
-	swap<int>(a, b);
-	std::cout << a << std::end;
-	std::cout << b << std::endl;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a,b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a,b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c,d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c,d) << std::endl;
 
 	return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 13:33:09 by akeryan           #+#    #+#             */
-/*   Updated: 2024/06/13 13:36:31 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/06/13 13:52:58 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,26 @@ void swap(T &a, T &b)
 	temp = a;
 	a = b;
 	b = temp;
+}
+
+template <typename T>
+T min(T &a, T &b)
+{
+	if (a < b)
+		return a;
+	if (b < a)
+		return b;
+	return b;
+}
+
+template <typename T>
+T max(T &a, T &b)
+{
+	if (a > b)
+		return a;
+	if (b > a)
+		return b;
+	return b;
 }
 
 #endif
